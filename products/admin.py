089ffaf3b8952b144c81cbcb945481ccf86cb1c3
Mojtaba Rezaei <mojtaba.rezaei7766@gmail.com>
2026-08-04 10:23:53 +0330

    Customize Django admin for products

--- products/admin.py
+++ products/admin.py
@@ -1,16 +1,78 @@
 from django.contrib import admin
+from .models import Category, Brand, Product, ProductImage, Review
 
-# Register your models here.
-from .models import (
-    Category,
-    Brand,
-    Product,
-    ProductImage,
-    Review,
-)
 
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(Product)
-admin.site.register(ProductImage)
-admin.site.register(Review)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ["id", "name", "slug"]
+
+    prepopulated_fields = {
+        "slug": ("name",)
+    }
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+
+    list_display = ["id", "name"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "id",
+        "name",
+        "category",
+        "brand",
+        "price",
+        "stock",
+        "available",
+        "created_at",
+    ]
+
+    list_filter = [
+        "category",
+        "brand",
+        "available",
+    ]
+
+    search_fields = [
+        "name",
+        "description",
+    ]
+
+    ordering = [
+        "-created_at",
+    ]
+
+    prepopulated_fields = {
+        "slug": ("name",)
+    }
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "id",
+        "product",
+        "image",
+    ]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "id",
+        "product",
+        "user",
+        "rating",
+        "created_at",
+    ]
+
+    list_filter = [
+        "rating",
+    ]
