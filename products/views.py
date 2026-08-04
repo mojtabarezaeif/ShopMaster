3a0a38bb3b8dcf38ca753f2c3fe325d678aae488
Mojtaba Rezaei <mojtaba.rezaei7766@gmail.com>
2026-08-04 11:27:11 +0330

    Add search filter sort and pagination

--- products/views.py
+++ products/views.py
@@ -5,6 +5,12 @@ from django.views.generic import ListView, DetailView
 from .models import Product
 
 
+from django.views.generic import ListView
+from django.db.models import Q
+
+from .models import Product, Category, Brand
+
+
 class ProductListView(ListView):
 
     model = Product
@@ -15,9 +21,79 @@ class ProductListView(ListView):
 
     paginate_by = 8
 
-    queryset = Product.objects.filter(
-        available=True
-    )
+    def get_queryset(self):
+
+        queryset = Product.objects.filter(
+            available=True
+        )
+
+        search = self.request.GET.get("q")
+
+        if search:
+
+            queryset = queryset.filter(
+
+                Q(name__icontains=search)
+
+                |
+
+                Q(description__icontains=search)
+
+                |
+
+                Q(category__name__icontains=search)
+
+                |
+
+                Q(brand__name__icontains=search)
+
+            )
+
+        category = self.request.GET.get("category")
+
+        if category:
+
+            queryset = queryset.filter(
+                category__slug=category
+            )
+
+        brand = self.request.GET.get("brand")
+
+        if brand:
+
+            queryset = queryset.filter(
+                brand__id=brand
+            )
+
+        sort = self.request.GET.get("sort")
+
+        if sort == "newest":
+
+            queryset = queryset.order_by("-created_at")
+
+        elif sort == "price_low":
+
+            queryset = queryset.order_by("price")
+
+        elif sort == "price_high":
+
+            queryset = queryset.order_by("-price")
+
+        elif sort == "name":
+
+            queryset = queryset.order_by("name")
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context["categories"] = Category.objects.all()
+
+        context["brands"] = Brand.objects.all()
+
+        return context
 
 
 class ProductDetailView(DetailView):
