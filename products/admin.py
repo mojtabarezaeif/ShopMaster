298045fa5f6cc145add4843cb2ed9906252867c3
Mojtaba Rezaei <mojtaba.rezaei7766@gmail.com>
2026-08-04 10:15:17 +0330

    Add product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    Category,
+    Brand,
+    Product,
+    ProductImage,
+    Review,
+)
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Review)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,92 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    brand = models.ForeignKey(
+        Brand,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=200)
+
+    slug = models.SlugField(unique=True)
+
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    stock = models.PositiveIntegerField(default=0)
+
+    available = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="images"
+    )
+
+    image = models.ImageField(
+        upload_to="products/"
+    )
+
+    def __str__(self):
+        return self.product.name
+
+
+from django.contrib.auth.models import User
+
+class Review(models.Model):
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    rating = models.PositiveSmallIntegerField()
+
+    comment = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.product}"
