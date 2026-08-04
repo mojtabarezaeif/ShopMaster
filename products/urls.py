cfb9d62d2307a3b4f8788defb849f70819c195c1
Mojtaba Rezaei <mojtaba.rezaei7766@gmail.com>
2026-08-04 10:58:45 +0330

    Create product list and detail pages

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
+from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
+
+    path(
+        "",
+        ProductListView.as_view(),
+        name="product_list"
+    ),
+
+    path(
+        "<slug:slug>/",
+        ProductDetailView.as_view(),
+        name="product_detail"
+    ),
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Product
+
+
+class ProductListView(ListView):
+
+    model = Product
+
+    template_name = "products/product_list.html"
+
+    context_object_name = "products"
+
+    paginate_by = 8
+
+    queryset = Product.objects.filter(
+        available=True
+    )
+
+
+class ProductDetailView(DetailView):
+
+    model = Product
+
+    template_name = "products/product_detail.html"
+
+    context_object_name = "product"
+
+    slug_field = "slug"
+
+    slug_url_kwarg = "slug"
